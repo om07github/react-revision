8e0e931dc171186840d260c2331009ca59e9e650
om07github <mahalleom794@gmail.com>
2025-01-11 20:58:37 +0530

    contextApi

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,17 @@
+// Context api creation / global file
+
+// step1:-
+
+// create folder in src name context:-
+
+// create file userContext.js
+// const UserContext = React.createContext()
+// export default UserContext;//userconetxt(provider)
+
+// step 2:- create usercontextprovider.jsx file in context
+
+import React from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext;
